9fd7ec8bdb115ec23303c1fa125e2a8348841873
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-24 14:53:30 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
-EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+EC2_INSTANCE_ID=i-0d083ed9c674a59df
+EC2_AZ=us-east-2c
 sed -i "s/was deployed/was deployed on $EC2_INSTSNCE_ID in $EC2_AZ/g" /var/www/html/index.html
 chmod 664 /var/www/html/index.html
