d242139af20d89c8e00da766a125401e41e99584
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-26 12:39:39 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,7 @@
 #EC2_INSTANCE_ID=i-05a1b1a64f057190c
 #EC2_AZ=us-east-2c
 #sudo sed -i "s/was deployed/was deployed on $EC2_INSTSNCE_ID in $EC2_AZ/g" /var/www/html/index.html
+
+#關閉../scripts目錄下檔案的一般瀏覽權限，限制瀏覽
 sudo chmod 600 /var/www/html/scripts/*
 
