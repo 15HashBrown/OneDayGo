bd28df1524113820f6ad0f4593aa3e0bcaa23062
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-04-26 11:03:37 +0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/eth0/disable_ipv6'
-sudo chmod 600 /var/www/html/scripts/*
+sudo chmod -x /var/www/html/scripts/*
 sudo systemctl start httpd
